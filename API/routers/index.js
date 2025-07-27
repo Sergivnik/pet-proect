a1353eb31985c7dbf3127c5d350f91d68061ee21
Sergivnik <65278578+Sergivnik@users.noreply.github.com>
2025-07-27 18:56:42 +0300

    Update index.js

--- API/routers/index.js
+++ API/routers/index.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const APIRouter = require("./APIRouter.js");
 //const APIMongoDBRouter = require("./APIMongoDBRouter.js");
 
-//router.use("/oders", APIRouter);
+router.use("/oders", APIRouter);
 router.use("/API", APIRouter);
-//router.use("/customer", APIRouter);
+router.use("/customer", APIRouter);
 //router.use("/API_MONGODB", APIMongoDBRouter);
 
 module.exports = router;
